71094fad08c4560fde58c7c4d5df4ddd55b8be14
BalchaBe <balcha36@yahoo.com>
2023-07-18 06:40:58 -0700

    error to fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - prints the string "mainfunct" from a character array.
+ * main - prints the string "kebedetel"from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "mainfunct";
-	int k;
+	char c[] = "Kebedetel";
+	int i;
 
-	for (k = 0; k < 8; k++)
+	for (i = 0; i < 9; i++)
 	{
-		_putchar(c[k]);
+		_putchar(c[i]);
 	}
 	_putchar('\n');
 	return (0);
