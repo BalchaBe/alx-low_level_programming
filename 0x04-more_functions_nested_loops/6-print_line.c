9083181e2ee28cf1fa57f7bd853d39087d2a6b0e
BalchaBe <balcha36@yahoo.com>
2023-07-20 04:59:16 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+  * Draws a straight line according to parameter
+  * 
+  * Return: empty
+  */
+void print_line(int n)
+{
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (b = 0; b < n; b++)
+		{
+			_putchar(101);
+		}
+
+		_putchar('\n');
+	}
+}
