38596b0bcc8f1aefcd735be7ade7c8507e4e6a6d
BalchaBe <balcha36@yahoo.com>
2023-07-27 02:02:38 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,23 +9,22 @@
  * Return: void
  */
 
-void print_line(char *c, int s, int l)
+void print_buffer(char *b, int size)
 {
-int m, k;
-for (m = 0; m <= 9; m++)
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-if (m <= s)
-printf("%02x", c[l * 10 + m]);
-else
-printf("  ");
-if (m % 2)
-putchar(' ');
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
 }
-for (k = 0; k <= s; k++)
-{
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-putchar(c[l * 10 + k]);
 else
-putchar('.');
+{
+print_line(b, size % 10 - 1, i);
 }
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
