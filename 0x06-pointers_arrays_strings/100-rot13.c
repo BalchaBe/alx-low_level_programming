958948baef1e10c5f24b8ded985a8c9767c31de2
BalchaBe <balcha36@yahoo.com>
2023-07-27 03:07:35 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int m, n;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		for (n = 0; a[n] != '\0'; n++)
+		{
+			if (s[m] == a[n])
+			{
+				s[m] = b[n];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
